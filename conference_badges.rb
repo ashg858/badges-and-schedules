54a9c6675efda32e07c1ed28ae780aebbfb1ffcb
ashg858 <aguidry27598@gmail.com>
2020-12-19 12:59:54 -0600

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,8 @@ def badge_maker(name)
   end
 
 def batch_badge_creator(attendees)
-    attendeess.map do |person|
-      badge_maker(person)
+    attendees.map.with_index(1) do |person|
+      "Hello, my name is #{person}."
     end
 end
 
